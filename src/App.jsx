4346febe28b5af63dee3431f28e9a2d817ea0147
Anmy Moreno <nayle2701@gmail.com>
2022-08-01 21:53:02 -0400

    star2152

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import Card_character from './assets/componets/Card_character'
+
+
+//https://gateway.marvel.com:443/v1/public/characters?apikey=
+//public key: 195ba63017e35e99119358fa915e70c0
+//private key:becb1f95d5364c2b26daac5a5bc4a0b27ef346a0
+//ts 1
+// 1becb1f95d5364c2b26daac5a5bc4a0b27ef346a0195ba63017e35e99119358fa915e70c0
+//hash:f985a8b7531eeffb721c3da8523f190b
+
+function App() {
+
+  const [character, setCharacter] = useState([])
+  
+  useEffect (()=>{
+    axios.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=195ba63017e35e99119358fa915e70c0&hash=f985a8b7531eeffb721c3da8523f190b')
+      .then(res=>{
+        setCharacter(res.data.data.results)
+      })
+      .catch(err => console.log(err))
+  },[])
+  
+  
+  console.log(character)
+
+  return (
+    <div className="App">
+    <h1>Marvel</h1> 
+    <Card_character character={character} /> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/componets/Card_character.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card_character = ({character}) => {
+
+   // console.log(character)
+  return (
+     //{ character.map(per => (
+       //  <div className='card' key={character.id}>
+       //      <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="" />
+        //     <div className="card_body">
+        //         <div className="card_text">
+        //             <p>Esta son las especificaciones</p>
+        //         </div>
+        //     </div>
+        // </div>
+       //  ))
+
+   //  }
+  <div></div>
+   )
+}
+
+export default Card_character
